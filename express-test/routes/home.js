ecc1a8e6e7dfca35258ae2d28dbe33b20fd96dbf
Parker Johnson <carverj95@gmail.com>
2022-02-01 20:46:51 -0500

    sdf

--- express-test/routes/home.js
+++ express-test/routes/home.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let router = express.Router()
 
-router.get('/test', async (req, res) =>
+router.get('/', async (req, res) =>
 {
     console.log("sending route")
     res.sendFile("index.html")
