cade2f15121a58404afb77d682d47c4e941682d2
parkerjohnson0 <74883548+parkerjohnson0@users.noreply.github.com>
2022-02-04 13:29:57 -0500

    Update game.js

--- express-test/public/game.js
+++ express-test/public/game.js
@@ -167,8 +167,8 @@ function sendClientState()
     {
         let client = playersList.find(x => x.id == socketID|| x.id == 0)
         // let clientJSON = JSON.stringify(client)
-        socket.emit("clientData", JSON.stringify(client))
         if (currframe % 2 == 0){
+        socket.emit("clientData", JSON.stringify(client))
         socket.emit("clientMouseData",{"mouseX": mouseX, "mouseY": mouseY,"id":socket.id})
         // console.log(clientJSON)
         }
